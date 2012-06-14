420248f785cda3490812fe7536db51d6c2e49208
Dustin Einglett <x13thangelx@gmail.com>
2012-06-13 19:36:25 -0500

    call liquid.mk

--- device.mk
+++ device.mk
@@ -27,3 +27,4 @@ PRODUCT_COPY_FILES := \
 
 $(call inherit-product, device/motorola/omap34com/device.mk)
 $(call inherit-product-if-exists, vendor/motorola/droid2we/droid2we-vendor.mk)
+$(call inherit-product, device/motorola/droid2we/liquid.mk)
